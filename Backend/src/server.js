3a3782ea0e8d7c457955b7b921b8e20c076b684c
Tumelo Meso <tumelomeso@icloud.com>
2026-03-03 00:33:54 +0200

    Create the server with the first base end point of the server

---
+++ Backend/src/server.js
@@ -0,0 +1,25 @@
+//Importing the express library which allows us to receive and handle HTTP requests to our server
+import express from "express"
+
+
+//Creating an instance of the an object of the express package which will be used to allows us to listen for requests
+//This step allows to have access to the  express package 
+const app = express()
+
+//Local port to run the server
+const PORT = process.env.PORT || 8080
+
+
+app.use(express.json())
+
+//Creating the base endpoint 
+//The frontend developers can use the status code 200 as a sign that they have successful connect to the server, they can then deliver the homepage or login page to the client 
+app.get("/", (req,res)=>{
+    res.status(200).json({message:"Successfully connected to the server"})
+})
+
+
+//Start listening to incoming request from our local PORT 8080
+app.listen(PORT, ()=>{
+    console.log(`Server has started on PORT ${PORT}`)
+})
